9960b2044ea0bed5c61e914486abde9339af8249
Matias Nahuel Ierace <matias.ierace@gmail.com>
2023-05-24 16:42:01 -0300

    super bloqueado con ver detalle

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,4 +1,4 @@
-/* import './ItemDetailContainer.css'
+import './ItemDetailContainer.css'
 import { useState, useEffect } from 'react'
 import ItemDetail from '../ItemDetail/ItemDetail'
 import { useParams } from 'react-router-dom'
@@ -37,64 +37,5 @@ const ItemDetailContainer = () => {
         </div>
     )
 }
-
-export default ItemDetailContainer;
- */
-
-import { useContext } from "react";
-import { useLocation } from "react-router-dom";
-import ItemCount from '../ItemCount/ItemCount'
-import { CartContext} from '../../context/CartContext'
-import "./ItemDetailContainer.css"
-
-import ItemDetail from '../ItemDetail/ItemDetail'
-
-  const ItemDetailContainer = ({ selected }) => {
-  const { state } = useLocation();
-  const { image,name,description,stock, price} = state;
-  const { addProduct } = useContext(CartContext);
-
-  const handlerCount = (count) => {
-    addProduct(count);
-  };
-
-/*   const handlerAddOrder = (count) => {
-    const item = {
-      name: title,
-      price: price,
-      total: count * price,
-    };
-
-    createOrder(item).then((result) => {
-      alert(`Se ha generadop la orden ${result}`);
-      console.log(result);
-    });
-  }; */
-
-/*   const handlerUpdateOrder = () => {
-    updateBatchOrders();
-    alert("Actualizado");
-  }; */
-
-  return (
-    <div className="item-detail">
-      <h1>{name}</h1>
-      <div>
-        <img src={image} width={300} height={300} />
-      </div>
-      <div>
-        <p>Descripcion: {description}</p>
-      </div>
-      <div>Precio: {price}</div>
-     {/*  <ItemCount
-        onChangeCount={(e) => handlerCount(e)}
-        onClickAddCart={(e) => handlerAddOrder(e)}
-        onClickUpdateCart={(e) => handlerUpdateOrder(e)}
-        maxCount={stock}
-        className={"item-detail__item-count"}
-      /> */}
-    </div>
-  );
-};
 
 export default ItemDetailContainer;
