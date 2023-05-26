19d8325154a699b3e69f63f45acc0bce823c0a35
Matias Nahuel Ierace <matias.ierace@gmail.com>
2023-05-26 11:24:48 -0300

    realizo checkout y el checkoutform del cliente

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import { CartProvider } from './context/CartContext';
 import Cart from './components/Cart/Cart'
+import Checkout from './components/Checkout/Checkout';
 
 function App(){
   return (
@@ -19,6 +20,7 @@ function App(){
             <Route path='/category/:categoryId' element={<ItemListContainer greeting={'Películas por categoria'}/>}/>
             <Route path='/item/:itemId' element={<ItemDetailContainer selected={true}/>}/>
             <Route path='/cart' element = {<Cart />} />
+            <Route path='/checkout' element = {<Checkout />}/>
             <Route path='*' element={<h1>404 NOT FOUND</h1>}/>
           </Routes>
         </CartProvider>
@@ -27,6 +29,4 @@ function App(){
   )
 }
 
-export default App;
-
-/* comentario para 2da Preentrega REACT*/
+export default App
