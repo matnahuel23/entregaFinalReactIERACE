aae801e38cb42feaddf2618b0d8cd51312c89554
Matias Nahuel Ierace <matias.ierace@gmail.com>
2023-05-29 12:37:01 -0300

    agregue y probe el correcto funcionamiento de .env agregado en gitignore

--- src/service/firebase/fireBaseConfig.jsx
+++ src/service/firebase/fireBaseConfig.jsx
@@ -1,20 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCamvjtuoVdUUtU5Kzu0OMJj4-MIVb0Wik",
-  authDomain: "matnahuel-coderhouse.firebaseapp.com",
-  projectId: "matnahuel-coderhouse",
-  storageBucket: "matnahuel-coderhouse.appspot.com",
-  messagingSenderId: "637386816719",
-  appId: "1:637386816719:web:7b5a5021b98e192bbf0bba",
-  measurementId: "G-J3BXCEFTYR"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
 }
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
+
 
 
 
